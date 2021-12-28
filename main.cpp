224975c34c8b24bba80f676e443570ba5e9e1e5e
Violina Toseva <violinatoseva@gmail.com>
2021-12-28 22:54:39 +0200

    Add basic menu

--- main.cpp
+++ main.cpp
@@ -2,6 +2,47 @@
 #include <fstream>
 #include <string>
 
+void main_menu() {
+
+    std::cout << "1. See numbers\n";
+    std::cout << "2. Change numbers\n";
+    std::cout << "3. See functions\n";
+    std::cout << "4. Change functions\n";
+    std::cout << "5. Calculate matrix\n";
+    std::cout << "6. Carry mode\n";
+    std::cout << "7. Save in a file\n";
+    std::cout << "8. Exit\n";
+
+    std::cout << "\n";
+
+    std::cout << "Choose option: \n";
+
+}
+
+void options(int userInput) {
+    switch (userInput) {
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+    case 5:
+        break;
+    case 6:
+        break;
+    case 7:
+        break;
+    default:
+        break;
+    }
+}
+
+void menu() {
+}
+
 int main()
 {
     std::fstream myFile1, myFile2;
