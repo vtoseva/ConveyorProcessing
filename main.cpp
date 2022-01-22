b36ab825f67e3be3a7109dd9f0d5a01adc907c46
Violina Toseva <violinatoseva@gmail.com>
2022-01-22 20:19:24 +0200

    Update matrix calculation functions

--- main.cpp
+++ main.cpp
@@ -235,55 +235,46 @@ double calc(double nmb, char op, double arg) {
 }
 
 void calculateMatrix(const std::vector<double>& nmb_vec, const std::vector<char>& op_vec, const std::vector<double>& arg_vec) {
-    int rows = nmb_vec.size();
-    int cols = arg_vec.size();
+    //dynamic matrix
+    int n = nmb_vec.size();
+    int m = arg_vec.size();
 
-    double** matrix = makeMatrix(rows, cols);
+    double** matrix = makeMatrix(n, m);
 
-    for (int row = 0; row < rows;row++) {
-        int i = 0;
-        for (int col = 0; col < cols; col++) {
-            if (op_vec[i] == '<' || op_vec[i] == '>') {
-                i++;
-            }
-
-            matrix[row][col] = calc(nmb_vec[row], op_vec[i], arg_vec[col]);
-
-            i++;
+    for (int row = 0; row < n;row++) {
+        for (int col = 0; col < m; col++) {
+            matrix[row][col] = calc(nmb_vec[row], op_vec[col], arg_vec[col]);
         }
     }
 
-    printMatrix(matrix, rows, cols);
+    printMatrix(matrix, n, m);
 
-    deleteMatrix(matrix, rows, cols);
+    saveInNewFile(matrix, n, m, 0);
+
+    deleteMatrix(matrix, n, m);
 }
 
 // calculates matrix with carry mode
 void carryMode(const std::vector<double>& nmb_vec, const std::vector<char>& op_vec, const std::vector<double>& arg_vec) {
-    int rows = nmb_vec.size();
-    int cols = arg_vec.size();
+    int n = nmb_vec.size();
+    int m = arg_vec.size();
 
-    double** matrix = makeMatrix(rows, cols);
+    double** matrix = makeMatrix(n, m);
 
-    for (int row = 0; row < rows;row++) {
-        int i = 0;
-        for (int col = 0; col < cols; col++) {
+    for (int row = 0; row < n;row++) {
+        for (int col = 0; col < m; col++) {
             if (col == 0) {
-                matrix[row][col] = calc(nmb_vec[row], op_vec[i], arg_vec[col]);
+                matrix[row][col] = calc(nmb_vec[row], op_vec[col], arg_vec[col]);
             }
-            else matrix[row][col] = calc(nmb_vec[row] + matrix[row][col - 1], op_vec[i], arg_vec[col]);
-
-            if (op_vec[i] == '<' || op_vec[i] == '>') {
-                i++;
-            }
-
-            i++;
+            else matrix[row][col] = calc(nmb_vec[row] + matrix[row][col - 1], op_vec[col], arg_vec[col]);
         }
     }
 
-    printMatrix(matrix, rows, cols);
+    printMatrix(matrix, n, m);
 
-    deleteMatrix(matrix, rows, cols);
+    saveInNewFile(matrix, n, m, 1);
+
+    deleteMatrix(matrix, n, m);
 }
 
 int main()
